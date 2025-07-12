419b1a5cc0fba7193a498447c7b77ba3512eff29
Tulio <tulioluv@gmail.com>
2025-07-11 22:01:07 -0300

    feat: render deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://e-commerce-api-62yt.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
